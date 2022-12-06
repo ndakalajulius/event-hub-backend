2f53790993c2d1e5d91c44ef9cdc1e40ac558157
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-12-06 11:00:16 +0300

    routing done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :tickets
+    has_many :events, through: :tickets
+  
+    validates :username, presence: true, uniqueness: true
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :tickets,only:[:index,:show,:create]
+  resources :events
+  # resources :users
+  post"/login", to: "sessions#create"
+  delete"/logout", to: "sessions#destroy"
+
+  post"/signup", to: "users#create"
+
+  get"/me", to: "users#show"
+
+  get"/events", to: "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
