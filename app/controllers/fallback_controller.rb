e615592ade459cd7cef9c9f5e65ab7a742397b56
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-12-06 11:08:44 +0300

    updates on fallback controller

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,2 +1,10 @@
-class FallbackController < ApplicationController
-end
+# Controller logic: fallback requests for React Router.
+# Leave this here to help deploy your app later!
+class FallbackController < ActionController::Base
+
+    def index
+      # React app index page
+      render file: 'public/index.html'
+    end
+  end
+  
