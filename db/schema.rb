61c16495466ff7925c61e1859e8c85bc194e7491
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-12-06 15:40:03 +0300

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_110843) do
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.string "image"
-    t.string "description"
-    t.date "date"
-    t.string "location"
-    t.float "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tickets", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
